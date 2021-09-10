68be3526d38c781e3953368d2c1893a6f47d4d7f
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-10 14:34:37 -0500

    update

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ Write a JavaScript function that takes in two parameters, an array and a number 
 Example: If I pass in an array and the number 2, your function returns the first 2 elements in that arrays.
 
 Steps:
-1. Create an array and assign it to a variable called "array".
-2. Create a variable called "n" and assign it to 0 so it is a number.
-3. Create a function and pass in the array and "n".
-4. In the function, 
+1. Pass an array and the first n elements within that array as parameters in a function.
+2. Utilize the slice method to return the first n elements within the passed array.
+3. Implement the return keyword to show the first n elements of the passed array. 
 */
 
 
