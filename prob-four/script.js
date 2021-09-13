30e582a93a47bfb2924c157c03f0a73d034b579e
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-12 20:01:44 -0500

    update

--- prob-four/script.js
+++ prob-four/script.js
@@ -1,1 +1,12 @@
-let myArray = [1,2,2,4,5,4,7,8,7,3,6];
+function removeDuplicates(array) {
+    let newArray = [];
+    for (i = 0; i < array.length; i++) {
+        if (newArray.indexOf(array[i]) === -1) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+}
+
+
