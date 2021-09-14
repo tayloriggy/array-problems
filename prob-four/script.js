c8187d586d6789f25b336d3d5ee83c2a69015cb1
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-14 13:41:52 -0500

    update

--- prob-four/script.js
+++ prob-four/script.js
@@ -1,16 +1,23 @@
 function removeDuplicates(array) {
-    let newArray = [];
-    for (i = 0; i < array.length; i++) {
-        if (newArray.indexOf(array[i]) === -1) {
-            newArray.push(array[i]);
+    let arrayOfUniqueValues = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!arrayOfUniqueValues.includes(array[i])) {
+            arrayOfUniqueValues.push(array[i]);
         }
     }
 
-    return newArray;
+    return arrayOfUniqueValues;
 }
 
-/* If the index of current element in passed array is not yet in the empty newArray, 
-then it is pushed into the newArray. The indexOf method automatically removes duplicate
-values as it is looping through each element in the passed array.
-If the item is present more than once, the indexOf method returns the position of the first occurence.
+console.log(removeDuplicates([1, 2, 2, 3, 4, 5, 5, 8, 5, 78, 89, 78, 89, 89, 90, 90]));
+
+/*
+1. Define a function that takes in an array.
+2. Create a new array that is used to track the unique values that don't have duplicates.
+3. Loop through the array that is passed in.
+4. Check if array of unique values already includes current element we are looping through.
+5. If current element is already included in unique values array, move on to next element.
+6. If current element is not included, then push it into unique values array.
+7. Once loop is finished, return unique values array.
+
 */
