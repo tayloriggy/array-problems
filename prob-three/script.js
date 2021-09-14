bae9d42af48f58e93a38d77fc915c72957bf7c5e
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-14 13:52:51 -0500

    update

--- prob-three/script.js
+++ prob-three/script.js
@@ -1,3 +1,12 @@
+/*
+Steps:
+1. Create an array that has all integers.
+2. Create a new array that will sort the integers in numerical order.
+3. Use the array sorts method to do this.
+4. Return the new sorted array. 
+*/
+
+
 let myArray = [-3,8,7,6,5,-4,3,2,1];
 let sortedArray = myArray.sort(function(a, b){return a-b});
 console.log(sortedArray);
