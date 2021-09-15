ce03f845f165b64ce26a1d565b1adcf86eec768b
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-09-14 21:23:32 -0500

    update

--- prob-two/script.js
+++ prob-two/script.js
@@ -1,3 +1,12 @@
+/*
+Steps:
+1. Given a specific array, write a program that joins all elements into a string.
+2. Create a new array of strings and set it equal to the original array.
+3. Call the join method on the array myColor to make all elements into a string.
+4. Return the new array of strings. 
+
+*/
+
 let myColor=["Red", "Green", "White", "Black"];
 let colorString = myColor.join(", ");
 console.log(colorString);
